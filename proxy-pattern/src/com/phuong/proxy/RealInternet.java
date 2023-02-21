d6ea2609b09015de8daba8ae6d7f3ee8ce52762e
tranthienminh135 <67372816+tranthienminh135@users.noreply.github.com>
2023-02-21 14:34:14 +0700

    update

--- proxy-pattern/src/com/phuong/proxy/RealInternet.java
+++
@@ -1,8 +0,0 @@
-package com.phuong.proxy;
-
-public class RealInternet implements Internet {
-    @Override
-    public void connectTo(String host) {
-        System.out.println("Connecting to " + host);
-    }
-}
